3f0a8e8b0146cc086f774df5537a8c3dbd1d28ae
jklee <jaydeen@kakao.com>
2022-08-24 23:54:04 +0900

    scanner try catch

--- Chapter_02/ScannerEx.java
+++ Chapter_02/ScannerEx.java
@@ -8,13 +8,16 @@ import java.util.Scanner;
 
 class ScannerEx {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("두자리 정수를 하나 입력해주세요.>");
+            String input = scanner.nextLine();
+            int num = Integer.parseInt(input);
 
-        System.out.print("두자리 정수를 하나 입력해주세요.>");
-        String input = scanner.nextLine();
-        int num = Integer.parseInt(input);
-
-        System.out.println("입력내용 :"+input);
-        System.out.printf("num=%d%n", num);
+            System.out.println("입력내용 :"+input);
+            System.out.printf("num=%d%n", num);
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
